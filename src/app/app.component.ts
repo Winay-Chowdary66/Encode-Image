3028e344fb35c39ec12fbb946ddc66700f17fc7e
Winay-Chowdary66 <uec17107@rmd.ac.in>
2022-04-01 21:30:59 +0530

    Reverting Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'image-converter';
+  contact_url = 'https://github.com/Winay-Chowdary66';
+  contact() {
+    window.open(this.contact_url);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DragAndDropDirective } from './directives/dradanddrop.directive';
+import { UploadImageComponent } from './upload-image/upload-image.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, UploadImageComponent, DragAndDropDirective],
+  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
+  providers: [UploadImageComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
